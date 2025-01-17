2a6269159414ba601a1aade460cfcd432ed6c724
Egor673 <klunniy.egor@yandex.ru>
2025-01-17 19:03:13 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username="Андрей"
+title="Не забыть!"
+content="Не забыть про уроки плавания"
+status="Актуально"
+created_date="12-02-2025"
+issue_date="15-02-2025"
+print(С,username)
+print("Заголовок заметки-",title)
+print("Описание заметки-",content)
+print("Статус заметки-",status)
+print("Дата создания заметки-",created_date)
+print("Дата истечения заметки-",issue_date)
