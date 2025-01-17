9e57ae7c6c97fa85d0e64bd38e914d157ccbb4b5
Egor673 <klunniy.egor@yandex.ru>
2025-01-17 19:06:08 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,9 @@
+created_date="12-06-2020"
+issue_date="13-июня-2000"
+print(created_date)
+temp_created_date=print(created_date[0:5])
+print(issue_date)
+temp_issue_date=print(issue_date[0:7])
+
+
+
