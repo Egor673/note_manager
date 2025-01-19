ec18ced3ed31685ffdefb5d6adb693e60ec6fb39
Egor673 <klunniy.egor@yandex.ru>
2025-01-19 17:40:31 +0300

    Add files via upload

--- add_list.py
+++ add_list.py
@@ -1,24 +1,21 @@
-username=input("Введите ваше имя:")
-title=input("Заголовок заметки:")
-title1=input("Основные темы")
-title2=input("Персонажи")
-title3=input("Рекомендации для чтения")
-title4=[title1,title2,title3]
-print(title4[:3])
-content=input("Описание заметки:")
-status=input("Статус заметки:")
-created_date = input("Дата начала события(ДД-ММ-ГГГГ):")
-temp_created_date="12-11-2020"
-issue_date = input("Дата окончания события(ДД-ММ-ГГГГ):")
-temp_issue_date="14-11-2020"
-print("Введите ваше имя:",username)
-print("Заголовок заметки:",title)
-print("Описание заметки:",content)
-print("Статус заметки:",status)
-print("Дата начала события:",temp_created_date[0:5])
-print("Дата окончания события:",temp_issue_date[0:5])
-
-
-
-
-
+username=input("Введите ваше имя: ")
+title=input("Заголовок заметки: ")
+title1=input("1.Основные темы: ")
+title2=input("2.Персонажи: ")
+title3=input("3.Рекомендации для чтения: ")
+title4=[f"Основные темы: {title1}, "
+        f"Персонажи: {title2}, "
+        f"Рекомендации: {title3}"]
+content=input("Описание заметки: ")
+status=input("Статус заметки: ")
+created_date=input("Дата начала события(ДД-ММ-ГГГГ): ")
+itog_created_date=created_date[0:5]
+issue_date=input("Дата окончания события(ДД-ММ-ГГГГ): ")
+itog_issue_date=issue_date[0:5]
+print("Ваше имя: ",username)
+print("Заголовок заметки: ",title)
+print(title4)
+print("Описание заметки: ",content)
+print("Статус: ",status)
+print("Дата начала события: ",itog_created_date)
+print("Дата окончания события: ",itog_issue_date)
