71ac44c8e9004167ce1cc99cd9b4bcd81b373a3e
Egor673 <klunniy.egor@yandex.ru>
2025-01-19 23:35:40 +0300

    add_list.py

--- add_list.py
+++ add_list.py
@@ -3,9 +3,7 @@ title=input("Заголовок заметки: ")
 title1=input("1.Основные темы: ")
 title2=input("2.Персонажи: ")
 title3=input("3.Рекомендации для чтения: ")
-title4=[f"Основные темы: {title1}, "
-        f"Персонажи: {title2}, "
-        f"Рекомендации: {title3}"]
+title4=[title1,title2,title3]
 content=input("Описание заметки: ")
 status=input("Статус заметки: ")
 created_date=input("Дата начала события(ДД-ММ-ГГГГ): ")
@@ -14,7 +12,7 @@ issue_date=input("Дата окончания события(ДД-ММ-ГГГГ)
 itog_issue_date=issue_date[0:5]
 print("Ваше имя: ",username)
 print("Заголовок заметки: ",title)
-print(title4)
+print(f"1.Основные темы:  {title4[0]} 2.Персонажи: {title4[1]} Рекомендации для чтения: {title4[1]}")
 print("Описание заметки: ",content)
 print("Статус: ",status)
 print("Дата начала события: ",itog_created_date)
