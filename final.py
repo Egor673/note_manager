bed228a296b1a5f8902f3999a5f9b5c75acd949f
Egor673 <klunniy.egor@yandex.ru>
2025-01-19 23:37:50 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,18 +1,23 @@
 note=["Имя пользователя: ",
-      "Содержание заметки: ",
+      "Заголовок заметки: ",
+      ["Заголовок1: ","Заголовок2: "],
+      "Описание заметки: ",
       "Статус: ",
-      "Дата создания: ",
-      "Дата изменения: ",
-      ["Заголовок1","Заголовок2: "]]
+      "Дата начала(ДД-ММ-ГГГГ): ",
+      "Дата окончания(ДД-ММ-ГГГГ): "]
 note1=[input(note[0]),
 input(note[1]),
-input(note[2]),
+input(note[2][0]),
+input(note[2][1]),
 input(note[3]),
 input(note[4]),
-input(note[5])]
+input(note[5]),
+input(note[6])]
 print(note[0]+note1[0])
 print(note[1]+note1[1])
-print(note[2]+note1[2])
-print(note[3]+note1[3])
-print(note[4]+note1[4])
-print(f"Заголовки: ",note1[5])
+print(note[2][0]+note1[2])
+print(note[2][1]+note1[3])
+print(note[3]+note1[4])
+print(note[4]+note1[5])
+print(note[5]+note1[6][0:5])
+print(note[6]+note1[7][0:5])
