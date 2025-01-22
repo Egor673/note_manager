f323200dbe5f39faa0d9539ff702780087c7c36d
Egor673 <klunniy.egor@yandex.ru>
2025-01-22 16:47:54 +0300

    final.py

--- final.py
+++ final.py
@@ -1,23 +1,18 @@
-note=["Имя пользователя: ",
-      "Заголовок заметки: ",
-      ["Заголовок1: ","Заголовок2: "],
-      "Описание заметки: ",
-      "Статус: ",
-      "Дата начала(ДД-ММ-ГГГГ): ",
-      "Дата окончания(ДД-ММ-ГГГГ): "]
-note1=[input(note[0]),
-input(note[1]),
-input(note[2][0]),
-input(note[2][1]),
-input(note[3]),
-input(note[4]),
-input(note[5]),
-input(note[6])]
-print(note[0]+note1[0])
-print(note[1]+note1[1])
-print(note[2][0]+note1[2])
-print(note[2][1]+note1[3])
-print(note[3]+note1[4])
-print(note[4]+note1[5])
-print(note[5]+note1[6][0:5])
-print(note[6]+note1[7][0:5])
+username=input("Введите ваше имя: ")
+title=input("Заголовок заметки: ")
+content=input("Описание заметки: ")
+status=input("Статус заметки: ")
+created_date=input("Дата начала события(ДД-ММ-ГГГГ): ")
+issue_date=input("Дата окончания события(ДД-ММ-ГГГГ): ")
+note=[username,
+      [title],
+      content,
+      status,
+      created_date,
+     issue_date]
+print(f"Имя пользователя: {note[0]} "
+      f"\nЗаголовок заметки: {note[1][0]} "
+      f"\nОписание заметки: {note[2]}"
+      f"\nСтатус заметки: {note[3]}"
+      f"\nДата начала: {note[4][:5]}"
+      f"\nДата окончания: {note[5][:5]}")
